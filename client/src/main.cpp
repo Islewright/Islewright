594a37832f2bdea233cb18ab38cb448e70eaf679
Chris Ohk <utilforever@gmail.com>
2026-07-03 13:52:52 +0900

    chore: resolve feedbacks by CodeRabbit

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -3,19 +3,26 @@
 
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
-
 #include <cstdlib>
+#include <iostream>
 
 using namespace islewright::config;
 
 int main(int argc, char* argv[])
 {
+    // Print the project name and version
+    std::cout << islewright::common::project_name() << " client "
+              << islewright::common::project_version() << '\n';
+
     // Initialize SDL3
-    SDL_Init(SDL_INIT_VIDEO);
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not initialize SDL: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
 
     // Create an application window with the following settings
-    SDL_Window* window =
-        SDL_CreateWindow(WINDOW_TITLE, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_OPENGL);
+    SDL_Window* window = SDL_CreateWindow(WINDOW_TITLE, WINDOW_WIDTH, WINDOW_HEIGHT,
+                                          SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
 
     // Check that the window was successfully created
     if (window == nullptr) {
@@ -30,8 +37,13 @@ int main(int argc, char* argv[])
         SDL_Event event;
 
         while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_EVENT_QUIT) {
+            switch (event.type) {
+            case SDL_EVENT_QUIT:
+            case SDL_EVENT_WINDOW_CLOSE_REQUESTED:
                 done = true;
+                break;
+            default:
+                break;
             }
         }
 
