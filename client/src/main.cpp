2176472ab1a0ec32d44a573762bd9c95423b0141
Chris Ohk <utilforever@gmail.com>
2026-07-03 11:50:07 +0900

    feat: set up initial SDL windowing

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,10 +1,27 @@
 #include "islewright/common/version.hpp"
 
-#include <iostream>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
-int main()
+int main(int argc, char* argv[])
 {
-	std::cout << islewright::common::project_name() << " client "
-		<< islewright::common::project_version() << '\n';
-	return 0;
+    // Initialize SDL3
+    SDL_Init(SDL_INIT_VIDEO);
+
+    // Create an application window with the following settings
+    SDL_Window* window = SDL_CreateWindow("Islewright", 800, 600, SDL_WINDOW_OPENGL);
+
+    // Check that the window was successfully created
+    if (window == nullptr) {
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    // Close and destroy the window
+    SDL_DestroyWindow(window);
+
+    // Clean up
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
 }
