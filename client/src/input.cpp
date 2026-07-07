c2d5784842fcbcd00a65973dacdabfd8462b62c2
Chris Ohk <utilforever@gmail.com>
2026-07-07 11:08:30 +0900

    feat: add code to handle mouse inputs

--- client/src/input.cpp
+++ client/src/input.cpp
@@ -6,6 +6,11 @@ void InputState::BeginFrame() noexcept
 {
     m_keysPressed.fill(false);
     m_keysReleased.fill(false);
+
+    m_mouseDeltaX = 0.0f;
+    m_mouseDeltaY = 0.0f;
+    m_wheelX = 0.0f;
+    m_wheelY = 0.0f;
 }
 
 void InputState::HandleEvent(const SDL_Event& event) noexcept
@@ -26,6 +31,33 @@ void InputState::HandleEvent(const SDL_Event& event) noexcept
         m_keysReleased[event.key.scancode] = m_keysDown[event.key.scancode];
         m_keysDown[event.key.scancode] = false;
         break;
+    case SDL_EVENT_MOUSE_MOTION:
+        m_mouseX = event.motion.x;
+        m_mouseY = event.motion.y;
+        m_mouseDeltaX += event.motion.xrel;
+        m_mouseDeltaY += event.motion.yrel;
+        break;
+
+    case SDL_EVENT_MOUSE_BUTTON_DOWN:
+    case SDL_EVENT_MOUSE_BUTTON_UP:
+        if (event.button.button > 0 && event.button.button <= CNT_MOUSE_BUTTON_COUNT) {
+            std::size_t index = static_cast<std::size_t>(event.button.button - 1);
+
+            if (event.type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
+                m_mousePressed[index] = !m_mouseDown[index];
+                m_mouseDown[index] = true;
+            } else {
+                m_mouseReleased[index] = m_mouseDown[index];
+                m_mouseDown[index] = false;
+            }
+        }
+        break;
+
+    case SDL_EVENT_MOUSE_WHEEL:
+        m_wheelX += event.wheel.x;
+        m_wheelY += event.wheel.y;
+        break;
+
     default:
         break;
     }
--- client/src/input.hpp
+++ client/src/input.hpp
@@ -1,8 +1,8 @@
 #ifndef ISLEWRIGHT_INPUT_HPP
 #define ISLEWRIGHT_INPUT_HPP
 
-#include <array>
 #include <SDL3/SDL_events.h>
+#include <array>
 
 namespace islewright::input {
 
@@ -15,11 +15,24 @@ class InputState
     bool QuitRequested() const noexcept;
 
   private:
+    static constexpr std::size_t CNT_MOUSE_BUTTON_COUNT = 8;
+
     bool m_quitRequested = false;
 
     std::array<bool, SDL_SCANCODE_COUNT> m_keysDown{};
     std::array<bool, SDL_SCANCODE_COUNT> m_keysPressed{};
     std::array<bool, SDL_SCANCODE_COUNT> m_keysReleased{};
+
+    std::array<bool, CNT_MOUSE_BUTTON_COUNT> m_mouseDown{};
+    std::array<bool, CNT_MOUSE_BUTTON_COUNT> m_mousePressed{};
+    std::array<bool, CNT_MOUSE_BUTTON_COUNT> m_mouseReleased{};
+
+    float m_mouseX = 0.0f;
+    float m_mouseY = 0.0f;
+    float m_mouseDeltaX = 0.0f;
+    float m_mouseDeltaY = 0.0f;
+    float m_wheelX = 0.0f;
+    float m_wheelY = 0.0f;
 };
 
 } // namespace islewright::input
